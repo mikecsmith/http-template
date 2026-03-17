b3fc44ef537a2574b97be212219b48e3200c6ffd
mikecsmith <mike.c.smith85@gmail.com>
2026-03-17 00:20:27 +0000

    feat: add middleware skeleton for hydrating logger context and utils for chaining middleware

--- cmd/server/routes.go
+++ cmd/server/routes.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	"net/http"
+	"slices"
 
 	"github.com/mikecsmith/httplab/internal/handle"
+	"github.com/mikecsmith/httplab/internal/middleware"
 )
 
+type middlewareChain []func(http.Handler) http.Handler
+
+func (c middlewareChain) thenFunc(h http.HandlerFunc) http.Handler {
+	return c.then(h)
+}
+
+func (c middlewareChain) then(h http.Handler) http.Handler {
+	for _, mw := range slices.Backward(c) {
+		h = mw(h)
+	}
+	return h
+}
+
 func addRoutes(mux *http.ServeMux) {
-	mux.Handle("GET /hello", handle.Hello())
+	baseChain := middlewareChain{middleware.LoggerContext}
+	mux.Handle("GET /hello", baseChain.thenFunc(handle.Hello()))
 }
